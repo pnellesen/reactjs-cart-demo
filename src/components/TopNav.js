ce0905637ac967daca8ef4136a5707978f152f79
pn4242 <pn4242@MOCDTL01pn4242.ITServices.sbc.com>
2018-06-04 11:11:45 -0500

    Fix for navbar rerendering due to toggle() function when links were selected in large viewports.

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -32,6 +32,7 @@ export default class TopNav extends Component {
 	constructor(props) {
 		super(props);
 		this.toggle = this.toggle.bind(this);
+                this.toggleCollapse = this.toggleCollapse.bind(this);
 			this.state = {
 					isOpen: false
 			};
@@ -43,22 +44,28 @@ export default class TopNav extends Component {
 	      isOpen: !this.state.isOpen
 	    });
 	  }
+          
+        toggleCollapse() {// Use this on links - will only cause rerender if in small viewports. If in large viewport, isOpen will always be false
+            if (this.state.isOpen) {
+                this.setState({isOpen: !this.state.isOpen});
+            }
+        }
 	
 	render () {
 		
 		return (
 			<div>
 	        <Navbar color="dark" dark expand="md">
-						<NavbarBrand tag={Link} to="/" onClick={this.toggle}>Mobile Web App</NavbarBrand>
+                  <NavbarBrand tag={Link} to="/" onClick={this.toggleCollapse}>Mobile Web App</NavbarBrand>
 	          <NavbarToggler onClick={this.toggle} />
 	          <Collapse isOpen={this.state.isOpen} navbar>
 	            <Nav className="" navbar>{
-								this.props.navItems.map((item, i) => 
-									<NavItem key={i}>
-										<NavLink tag={Link} to={item.navTo} onClick={this.toggle}>{item.navText}</NavLink>
-									</NavItem>
-								)
-							}</Nav>
+                            this.props.navItems.map((item, i) => 
+                                    <NavItem key={i}>
+                                            <NavLink tag={Link} to={item.navTo} onClick={this.toggleCollapse}>{item.navText}</NavLink>
+                                    </NavItem>
+                            )
+                    }</Nav>
 	          </Collapse>
 	        </Navbar>
 	      </div>
